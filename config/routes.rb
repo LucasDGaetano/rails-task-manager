e56b1ad8577f28c19ddf9994b520810f0dc15cf7
Lucas De Gaetano <lucasdgpro@gmail.com>
2023-11-14 11:06:10 +0100

    Exercice done, refacto done, front-end NOT done

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,20 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  # get 'tasks' => 'tasks#index', as: :index
+
+  # get 'tasks/new' => 'tasks#new', as: :new_task
+
+  # post 'tasks' => 'tasks#create'
+  # delete 'tasks/:id' => 'tasks#destroy'
+
+  # get 'tasks/:id/edit' => 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id' => 'tasks#update'
+
+  # get 'tasks/:id' => 'tasks#show', as: :task
+
+  resources :tasks
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
